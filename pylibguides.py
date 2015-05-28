0d31414d51a1a5a0cf3e0c85150148c1496897a5
Mark <mbagget1@utk.edu>
2015-05-27 23:11:30 -0400

    Add script that reads api key and creates dc records

---
+++ pylibguides.py
@@ -0,0 +1,30 @@
+from urllib2 import urlopen
+import json
+
+# Get API Key and Build URL String
+f = open('apikey.txt', 'r')
+apikey = f.read()
+fullURL = apikey.rstrip('\n') + '&guide_types=3&status=1&expand=owner&expand=subjects,owner'
+
+req = urlopen(fullURL).read()
+outfile= json.loads(req)
+
+for record in outfile:
+	print 'Creating record ' + record['id']
+	xml = open('temp/' + record['id'] + '.xml'.format(record['id']), 'w')
+	xml.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+	xml.write('<oai_dc:dc xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">\n')
+	xml.write('\t<dc:title>' + record['name'] + '</dc:title>\n')
+	xml.write('\t<dc:identifier>' + record['url'] + '</dc:identifier>\n')
+	xml.write('\t<dc:description>' + record['description'] + '</dc:description>\n')
+	xml.write('\t<dc:creator>' + record['owner']['first_name'] + " " + record['owner']['last_name'] + '</dc:creator>\n')
+	if 'subjects' in record:
+		numOfSubs = len(record['subjects'])
+		for x in range(0,numOfSubs):
+			xml.write('\t<dc:subject>' + record['subjects'][x]['name'] + '</dc:subject>\n')
+	xml.write('\t<dc:publisher>The University of Tennessee Libraries, Knoxville</dc:publisher>\n')
+	xml.write('\t<dc:type>text</dc:type>\n')
+	xml.write('\t<dc:type>interactive</dc:type>\n')
+	xml.write('\t<dc:format>text/HTML</dc:format>\n')
+	xml.write('</oai_dc:dc>')
+	xml.close()
